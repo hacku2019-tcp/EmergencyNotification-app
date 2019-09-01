3007f8ad203bf67fb36290f8cbbcd74b27383e27
Crow314 <crow31415@gmail.com>
2019-09-02 08:50:07 +0900

    通知送信機能作成

--- app/src/main/java/net/crow31415/emergencyNotification_app/activity/SendNotificationActivity.java
+++ app/src/main/java/net/crow31415/emergencyNotification_app/activity/SendNotificationActivity.java
@@ -2,15 +2,30 @@ package net.crow31415.emergencyNotification_app.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import net.crow31415.emergencyNotification_app.R;
+import net.crow31415.emergencyNotification_app.util.HTTPSUtility;
 
 public class SendNotificationActivity extends AppCompatActivity {
+
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_notification);
+
+        preferences = getSharedPreferences("net.crow31415.emergencyNotification_app.preferences", MODE_PRIVATE);
+        sendNotification();
+    }
+
+    public void sendNotification(){
+        String userId = preferences.getString("id", null);
+
+        String post = "apiType=notification" + "&userId=" + userId;
+        HTTPSUtility https = new HTTPSUtility();
+        https.execute("https://hacku.dragon-egg.org/api", post);
     }
 }
